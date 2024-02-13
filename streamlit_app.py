4b0fc8bf416609e2c19ed148b0ba6684843e00cd
Atharva Pangerkar <84865244+Atharvap2000@users.noreply.github.com>
2024-02-13 16:03:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,43 @@
-import streamlit as
+import streamlit as st
+def main():
+    st.title("Test Automation")
 
-st.write("Hello World")
+    # Get or create session_state
+    session_state = st.session_state
+    if "user_authenticated" not in session_state:
+        session_state.user_authenticated = False
+
+    if not session_state.user_authenticated:
+        # Login
+        st.sidebar.subheader("Login")
+        username = st.sidebar.text_input("Username:")
+        password = st.sidebar.text_input("Password:", type="password")
+        login_button = st.sidebar.button("Login")
+
+        if login_button:
+            if authenticate(username, password):
+                session_state.user_authenticated = True
+            else:
+                st.sidebar.error("Invalid username or password. Please try again.")
+    else:
+        # Provide user with different options after successful login
+        options = ["", "Migration", "Object Creation", "Documentation"]
+        selected_option = st.selectbox("Select an option:", options)
+
+        # Perform different functions based on the selected option
+        if selected_option == "Migration":
+            st.write("Migration selected")
+        elif selected_option == "Object Creation":
+             st.write("Object creation selected")
+        elif selected_option == "Documentation":
+            st.write("RD selected")
+
+        # Logout
+        logout_button = st.sidebar.button("Logout")
+        if logout_button:
+            session_state.user_authenticated = False
+
+# Your authentication and input functions go here
+
+if __name__ == "__main__":
+    main()
